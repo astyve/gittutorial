f59de5953cde5e3ce8a8d8110f2ef3dd752bc9a0
asty@ntnu.no <asty@ntnu.no>
2021-02-25 08:34:37 +0100

    Initial version

---
+++ src/main/java/no/ntnu/idata2001/gittutorial/Person.java
@@ -0,0 +1,13 @@
+package no.ntnu.idata2001.gittutorial;
+
+public class Person {
+  private String name;
+
+  public Person(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+}

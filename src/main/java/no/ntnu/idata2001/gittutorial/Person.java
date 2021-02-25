a9d882855a11c6ac5e8829331ced6d65aaf430a9
asty@ntnu.no <asty@ntnu.no>
2021-02-25 10:32:12 +0100

    Added a field for e-mail

--- src/main/java/no/ntnu/idata2001/gittutorial/Person.java
+++ src/main/java/no/ntnu/idata2001/gittutorial/Person.java
@@ -3,6 +3,7 @@ package no.ntnu.idata2001.gittutorial;
 public class Person {
   private String fullName;
   private int age;
+  private String eMail;
 
   public Person(String name, int age) {
     this.fullName = name;
